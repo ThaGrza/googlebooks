2cc07f5aeac9638b8315a9029464ce9ea083af00
Andrew Griswold <andrewgriswold7@gmail.com>
2019-08-12 19:06:07 -0500

    sqwe

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -5,7 +5,7 @@ function Title() {
         <div className="row ">
             <div className="col-md-8 mx-auto">
                 <div className="jumbotron">
-                    <h1 className="display-4">(React) Google Book Search</h1>
+                    <h1 className="display-4">Google Book Search</h1>
                     <p className="lead">Search for and Save Books of Interest</p>
                 </div>
             </div>
